aed22563ae6e8496a244e1b94c08e96342b65916
Astemir-Gurizhev <gurizhev.astemir@gmail.com>
2025-02-27 00:54:32 +0300

    Change main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,14 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import styles from './page.module.scss'
 
 export default function Home() {
-	return <main className={styles.main}></main>
+	return (
+		<main className={styles.main}>
+			<Image src='/book.webp' alt='' width={150} height={150} />
+			<Link className={styles.btn} href='/'>
+				Start!
+			</Link>
+		</main>
+	)
 }
--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -2,7 +2,7 @@ import styles from './Footer.module.scss'
 export default function Footer() {
 	return (
 		<div className={styles.footer}>
-			<p>Astemir Gurizhev &copy; 2025 </p>
+			<p>Mathify | Astemir Gurizhev &copy; 2025 </p>
 		</div>
 	)
 }
